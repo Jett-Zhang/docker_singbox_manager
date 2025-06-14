cb4d36d81e7bf6ed2e0cf96f30da7efffbe8fdf8
Jett-Zhang <yfszhj@163.com>
2025-06-14 16:56:58 +0800

    no message

--- quickstart.sh
+++ quickstart.sh
@@ -12,17 +12,17 @@ files=(
   singbox_manager.sh
 )
 
+# 创建目标目录
+mkdir -p docker_singbox_manager/sh
+
 # 下载所有脚本
 for file in "${files[@]}"; do
   # echo "正在下载 $file ..."
-  curl -fsSL -o "$file" "https://raw.githubusercontent.com/Jett-Zhang/docker_singbox_manager/main/$file"
+  curl -fsSL -o "docker_singbox_manager/sh/$file" "https://raw.githubusercontent.com/Jett-Zhang/docker_singbox_manager/main/$file"
 done
 
 # 给所有脚本加执行权限
-chmod +x *.sh
+chmod +x docker_singbox_manager/sh/*.sh
 
 # 运行主程序
-./main.sh
-
-# 可选：执行完自动删除所有sh文件
-rm -f *.sh
+./docker_singbox_manager/sh/main.sh
